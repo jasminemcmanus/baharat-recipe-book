82b4b3b5c82917957263487bf8a0fc7b57452333
Jasmine <129954981+jasminemcmanus@users.noreply.github.com>
2024-12-15 22:11:53 +1030

    initial draft

---
+++ script.js
@@ -0,0 +1,22 @@
+/* type effect */
+
+/*
+let speed = 90;
+let headerText = document.getElementById('type-effect');
+
+function typeEffect(element, speed) {
+	let text = element.innerHTML;
+	element.innerHTML = "";
+	
+	let i = 0;
+	let timer = setInterval(function() {
+    if (i < text.length) {
+      element.append(text.charAt(i));
+      i++;
+    } else {
+      clearInterval(timer);
+    }
+  }, speed);
+}
+
+typeEffect(headerText, speed) */
